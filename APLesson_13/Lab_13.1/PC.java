5e41b4158fece53a3ce3391f49cb9a72afd031a7
ivyhuang9 <ivyhuang9@gmail.com>
2017-03-02 09:11:32 -0800

    Edited Files

--- APLesson_13/Lab_13.1/PC.java
+++ APLesson_13/Lab_13.1/PC.java
@@ -3,6 +3,9 @@ public class PC extends GameSystem
 	public String systemInput(){
 		return "Keyboard and Mouse";
 	}
+	public String getPlatform(){
+		return "PC";
+	}
 	public String toString(){
 		return "Platform: " + getPlatform() +
 			"\nSerial #: " + getSerialNo() +
